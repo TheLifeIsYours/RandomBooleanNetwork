c8a5516777f4415b2517e4e061e885809fd7d4b8
thelifeisyours <tliyours@gmail.com>
2020-11-19 01:59:37 +0100

    initial commit

---
+++ out/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const RBN_Network_1 = require("./RBN_Network");
+console.log("Started project");
+const network = new RBN_Network_1.default();
+//# sourceMappingURL=index.js.map
