b2d8287fc1c436702fdfbbeacb953f5e0913502f
TheLifeIsYours <7der54@gmail.com>
2020-11-19 02:09:18 +0100

    git ignores

--- out/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RBN_Network_1 = require("./RBN_Network");
-console.log("Started project");
-const network = new RBN_Network_1.default();
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import {default as RBN} from './RBN_Network'
-
-console.clear()
-console.log("Started project")
-
-const network = new RBN({nodes: 20, siblings: 3, steps: 100})
